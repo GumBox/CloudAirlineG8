8da927f91b3e8e5f46f011b5beeb31c49f369f4a
falcon <haft2k@gmail.com>
2022-08-11 08:41:14 +0700

    change folder src to font-end

---
+++ fontend/Pages/Home.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+
+
+function Home() {
+
+    return (
+
+        <>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-sm-12 col-md-3 col-lg-3">
+                        <button className="btn btn-primary" data-bs-target="#collapseExample" data-bs-toggle="collapse" aria-expanded="false" aria-controls="collapseExample">
+                            OKe nhe
+                        </button>
+                        <div class="collapse" id="collapseExample">
+                            <div class="card card-body">
+                                Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+                            </div>
+                        </div>
+
+
+                    </div>
+                    <div className="col-md-6">
+                        fdkaf
+                    </div>
+
+                </div>
+
+
+
+            </div>
+        </>
+    )
+
+}
+export default Home;
