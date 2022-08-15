e7e6895fba961156ce12dfa063951d0db3722428
PhuongHoangLong <phuonghoanglong2001@gmail.com>
2022-08-15 17:37:08 +0700

    kkk
    kkk

--- fontend/App.js
+++ fontend/App.js
@@ -1,6 +1,8 @@
-import Layout from "./Components/Layout";
+import Layout from "./Pages/Layout";
 import Home from "./Pages/Home";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Service_Contact from "./Pages/Service_Contact";
+import Ticket from "./Pages/Ticket";
 
 
 function App() {
@@ -9,7 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-
+          <Route path="service_contact" element={< Service_Contact />} />
+          <Route path="ticket" element={< Ticket />} />
         </Route>
       </Routes>
     </BrowserRouter>
