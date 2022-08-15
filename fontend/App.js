fc01dd6a87bfa4eb197176c9714bbfc8fbb58d57
falcon <haft2k@gmail.com>
2022-08-15 18:00:34 +0700

    update file in folder font-end
    test ok

---
+++ fontend/App.js
@@ -0,0 +1,22 @@
+import Layout from "./Pages/Layout";
+import Home from "./Pages/Home";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Service_Contact from "./Pages/Service_Contact";
+import Ticket from "./Pages/Ticket";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="service_contact" element={< Service_Contact />} />
+          <Route path="ticket" element={< Ticket />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
