e9a32e9ae65bb710eeb3d5aff932ea4900f4100b
falcon <haft2k@gmail.com>
2022-08-15 17:45:21 +0700

    add model

---
+++ backend/model/Ticket.java
@@ -0,0 +1,38 @@
+/*
+ * GumBox Inc
+ * (c) 2022 GumBox Inc. Viet Nam
+ *
+ */
+package com.example.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author Blue Maxx
+ */
+@Entity
+public class Ticket {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column( name = "ticket_id")
+    private int ticketID;
+    
+    private String ticketPrice;
+    
+    private String ticketClass;
+    
+    private int seatNumber;
+    
+//    @ManyToOne
+//    @JoinColumn(name = "receiptid")
+//    private Receipt receiptID;
+}   
+
