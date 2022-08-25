e61f82d8a611cdf047e9023b61c68c42eb76bd0f
falcon <haft2k@gmail.com>
2022-08-25 15:48:50 +0700

    update database

---
+++ database/MySQL/plane.sql
@@ -0,0 +1,12 @@
+
+insert into plane (plane_id, plane_brand, plane_code, total_seat) values (1, 'Boeing', '777', 300);
+insert into plane (plane_id, plane_brand, plane_code, total_seat) values (2, 'Boeing', '777', 200);
+insert into plane (plane_id, plane_brand, plane_code, total_seat) values (3, 'Boeing', '777', 300);
+insert into plane (plane_id, plane_brand, plane_code, total_seat) values (4, 'Airbus', '320', 200);
+insert into plane (plane_id, plane_brand, plane_code, total_seat) values (5, 'Airbus', '320', 500);
+insert into plane (plane_id, plane_brand, plane_code, total_seat) values (6, 'Airbus', '320', 100);
+insert into plane (plane_id, plane_brand, plane_code, total_seat) values (7, 'Airbus', '320', 120);
+insert into plane (plane_id, plane_brand, plane_code, total_seat) values (8, 'Airbus', '320', 125);
+insert into plane (plane_id, plane_brand, plane_code, total_seat) values (9, 'Airbus', '380', 320);
+insert into plane (plane_id, plane_brand, plane_code, total_seat) values (10, 'Airbus', '380', 120);
+insert into plane (plane_id, plane_brand, plane_code, total_seat) values (11, 'Antonov', '380', 300);
